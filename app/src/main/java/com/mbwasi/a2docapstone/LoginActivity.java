a41fb9f093e58551e8234c8b94b5687752a1c1aa
Roman <romanmbwasi@gmail.com>
2020-03-12 16:06:34 -0400

    Started HTTP wotk on login activity

--- app/src/main/java/com/mbwasi/a2docapstone/LoginActivity.java
+++ app/src/main/java/com/mbwasi/a2docapstone/LoginActivity.java
@@ -3,8 +3,15 @@ package com.mbwasi.a2docapstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import pk.codebase.requests.HttpError;
+import pk.codebase.requests.HttpRequest;
+import pk.codebase.requests.HttpResponse;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,8 +22,36 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginPressed(View view) {
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+        HttpRequest request = new HttpRequest();
+        request.setOnResponseListener(new HttpRequest.OnResponseListener() {
+            @Override
+            public void onResponse(HttpResponse response) {
+                if (response.code == HttpResponse.HTTP_OK) {
+                    Log.e("LoginActivity", response.toJSONObject().toString());
+                    System.out.println(response.toJSONObject());
+
+                    SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString("LOGIN_TOKEN", "string value"); // Storing string
+                    editor.commit();
+
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        request.setOnErrorListener(new HttpRequest.OnErrorListener() {
+            @Override
+            public void onError(HttpError error) {
+                // There was an error, deal with it
+                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                Log.e("LoginActivity", error.toString());
+            }
+        });
+        request.get("https://jsonplaceholder.typicode.com/todos/1");
+
+
+
     }
 
 
