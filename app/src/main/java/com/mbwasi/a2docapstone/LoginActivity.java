7c24a97a619b4feaa5b24ee43f91a97e9f8ccac2
roman <romanmbwasi@gmail.com>
2020-03-12 17:15:13 -0400

    Added API login to LoginActivity.

--- app/src/main/java/com/mbwasi/a2docapstone/LoginActivity.java
+++ app/src/main/java/com/mbwasi/a2docapstone/LoginActivity.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import pk.codebase.requests.HttpError;
 import pk.codebase.requests.HttpRequest;
 import pk.codebase.requests.HttpResponse;
@@ -22,33 +25,79 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginPressed(View view) {
+//        HttpRequest request = new HttpRequest();
+//        request.setOnResponseListener(new HttpRequest.OnResponseListener() {
+//            @Override
+//            public void onResponse(HttpResponse response) {
+//                if (response.code == HttpResponse.HTTP_OK) {
+//                    Log.e("LoginActivity", response.toJSONObject().toString());
+//                    System.out.println(response.toJSONObject());
+//
+//                    if(TokenUtils.storeLoginToken("Token string goes here",getApplicationContext())){
+//                        //If token was succesfully stored continue to main page
+//                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+//                        startActivity(intent);
+//                    }
+//                }
+//            }
+//        });
+//        request.setOnErrorListener(new HttpRequest.OnErrorListener() {
+//            @Override
+//            public void onError(HttpError error) {
+//                // There was an error, deal with it
+//                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+//                Log.e("LoginActivity", error.toString());
+//            }
+//        });
+//        request.get("http://2doo.ca/api/login");
+//////////////
         HttpRequest request = new HttpRequest();
         request.setOnResponseListener(new HttpRequest.OnResponseListener() {
             @Override
             public void onResponse(HttpResponse response) {
                 if (response.code == HttpResponse.HTTP_OK) {
-                    Log.e("LoginActivity", response.toJSONObject().toString());
-                    System.out.println(response.toJSONObject());
 
-                    SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
-                    SharedPreferences.Editor editor = pref.edit();
-                    editor.putString("LOGIN_TOKEN", "string value"); // Storing string
-                    editor.commit();
+                    try {
+                        Log.e("LoginActivity", response.toJSONObject().toString());
 
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
+                        JSONObject jsonObject = response.toJSONObject().getJSONObject("success");
+                        String token =jsonObject.getString("token");
+                        Log.e("LoginActivity", "Token: "+token);
+                        if(TokenUtils.storeLoginToken("Token string goes here",getApplicationContext())){
+                            //If token was succesfully stored continue to main page
+                            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(intent);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                else if (response.code == HttpResponse.HTTP_UNAUTHORIZED) {
+                    Toast.makeText(LoginActivity.this, "Invalid Login", Toast.LENGTH_LONG).show();
                 }
             }
         });
         request.setOnErrorListener(new HttpRequest.OnErrorListener() {
             @Override
             public void onError(HttpError error) {
-                // There was an error, deal with it
                 Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
                 Log.e("LoginActivity", error.toString());
             }
         });
-        request.get("https://jsonplaceholder.typicode.com/todos/1");
+
+        //TODO: Get username and password from edit text
+        JSONObject json;
+        try {
+            json = new JSONObject();
+            json.put("email", "romanmbwasi@gmail.com");
+            json.put("password", "password");
+        } catch (JSONException ignore) {
+            return;
+        }
+        request.post("http://2doo.ca/api/login", json);
+
 
 
 
---
+++ app/src/main/java/com/mbwasi/a2docapstone/TokenUtils.java
@@ -0,0 +1,29 @@
+package com.mbwasi.a2docapstone;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+public abstract class TokenUtils {
+
+    public static final String DOO_PREFS = "2DOO_PREFS";
+    public static final String LOGIN_TOKEN = "LOGIN_TOKEN";
+
+    public static boolean storeLoginToken(String tokenString, Context context){
+        try {
+            SharedPreferences pref = context.getSharedPreferences(DOO_PREFS, 0); // 0 - for private mode
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString(LOGIN_TOKEN, tokenString); // Storing string
+            editor.commit();
+            return true;
+        }catch(Exception ex){
+            Log.e("TokenUtils",ex.toString());
+            return false;
+        }
+    }
+
+    public String getLoginToken(Context context){
+        SharedPreferences pref = context.getSharedPreferences(DOO_PREFS, 0); // 0 - for private mode
+        return pref.getString(LOGIN_TOKEN,null);
+    }
+}
