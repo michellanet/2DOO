c022505d4468f698a6971e47b3d61f5baefbe30c
Roman <romanmbwasi@gmail.com>
2020-03-12 11:11:08 -0400

    Wrapped About Activity in Nav Drawer

--- app/src/main/java/com/mbwasi/a2docapstone/About.java
+++ app/src/main/java/com/mbwasi/a2docapstone/About.java
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class About extends AppCompatActivity {
+public class About extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
