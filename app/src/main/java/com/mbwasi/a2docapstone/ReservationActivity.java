c7661516378e9f024e803552e2cf4cbaa06ff9a3
Deep Desai <Deepdesai36@gmail.com>
2020-04-04 12:29:00 -0400

    Michaels Parts Added: Can Create/Load Reservations

--- app/src/main/java/com/mbwasi/a2docapstone/ReservationActivity.java
+++ app/src/main/java/com/mbwasi/a2docapstone/ReservationActivity.java
@@ -1,6 +1,7 @@
 package com.mbwasi.a2docapstone;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.app.DatePickerDialog;
@@ -12,6 +13,9 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.mbwasi.a2docapstone.entity.ReservationProvider;
+
 import java.util.Calendar;
 
 
@@ -22,6 +26,7 @@ public class ReservationActivity extends BaseActivity {
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     private TextView timeInput;
     Calendar cal = Calendar.getInstance();
+    int year, month, day, hour, minute;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,9 +42,9 @@ public class ReservationActivity extends BaseActivity {
 
 // Process to get Current Date
         final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
 
         // Launch Date Picker Dialog
         DatePickerDialog dpd = new DatePickerDialog(ReservationActivity.this,
@@ -59,8 +64,8 @@ public class ReservationActivity extends BaseActivity {
 
     public void timePopup(View view){
         Calendar cal = Calendar.getInstance();
-        int hour = cal.get(Calendar.HOUR_OF_DAY);
-        int minute = cal.get(Calendar.MINUTE);
+        hour = cal.get(Calendar.HOUR_OF_DAY);
+        minute = cal.get(Calendar.MINUTE);
         final int meridian = cal.get(Calendar.AM_PM);
 
         TimePickerDialog timePickerDialog = new TimePickerDialog(
@@ -73,19 +78,136 @@ public class ReservationActivity extends BaseActivity {
                 //am_pm = "AM";
                 //else if (meridian == Calendar.PM)
                 //am_pm = "PM";
-                timeInput.setText(hourOfDay + " : " + minute + " Hours");
+                timeInput.setText(hourOfDay + " : " + minute);
             }
         }, hour, minute,android.text.format.DateFormat.is24HourFormat(ReservationActivity.this)
         );
         timePickerDialog.show();
     }
     public void makeReservation(View view) {
-        Toast.makeText(this, "Table for Party of: " + partySize.getSelectedItem().toString()
-                + "\n reserved Date: " + dateInput.getText()
-                + "\n reserved Time: " + timeInput.getText(), Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, ReservationHistoryListActivity.class);
-        startActivity(intent);
+        if (!dateInput.getText().toString().matches("") && !timeInput.getText().toString().matches("")) {
+
+            Toast.makeText(this, "Table for Party of: " + partySize.getSelectedItem().toString()
+                    + "\n reserved Date: " + dateInput.getText()
+                    + "\n reserved Time: " + timeInput.getText(), Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, ReservationHistoryListActivity.class);
+            ReservationProvider.ReservationItem item = new ReservationProvider.ReservationItem(
+                    partySize.getSelectedItem().toString(),
+                    dateInput.getText().toString(),
+                    timeInput.getText().toString() + " Hours",
+                    partySize.getSelectedItem().toString() + "-" + dateInput.getText().toString() + "-" + timeInput.getText().toString());
+            ReservationProvider.ITEMS.add(0, item);
+
+        /*
+        //save item as json to shared preferences
+        SharedPreferences sharedPreferences = getSharedPreferences("reservationPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(ReservationProvider.ITEMS);
+        editor.putString("reservationList", json);
+        editor.apply();*/
+
+//            Intent intent = new Intent(this, ReservationHistoryListActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "Please ensure all input fields are filled", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
 }
+
+
+
+//package com.mbwasi.a2docapstone;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//
+//import android.app.DatePickerDialog;
+//import android.app.TimePickerDialog;
+//import android.view.View;
+//import android.widget.DatePicker;
+//import android.widget.Spinner;
+//import android.widget.TextView;
+//import android.widget.TimePicker;
+//import android.widget.Toast;
+//
+//import java.util.Calendar;
+//
+//
+//public class ReservationActivity extends BaseActivity {
+//
+//    private TextView dateInput;
+//    private Spinner partySize;
+//    private DatePickerDialog.OnDateSetListener mDateSetListener;
+//    private TextView timeInput;
+//    Calendar cal = Calendar.getInstance();
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_reservation);
+//        dateInput = (TextView) findViewById(R.id.date);
+//        partySize = (Spinner) findViewById(R.id.partySize);
+//        timeInput = (TextView) findViewById(R.id.time);
+//
+//    }
+//
+//    public void calPopup(View view){
+//
+//// Process to get Current Date
+//        final Calendar c = Calendar.getInstance();
+//        int year = c.get(Calendar.YEAR);
+//        int month = c.get(Calendar.MONTH);
+//        int day = c.get(Calendar.DAY_OF_MONTH);
+//
+//        // Launch Date Picker Dialog
+//        DatePickerDialog dpd = new DatePickerDialog(ReservationActivity.this,
+//                new DatePickerDialog.OnDateSetListener() {
+//
+//                    @Override
+//                    public void onDateSet(DatePicker view, int year,
+//                                          int monthOfYear, int dayOfMonth) {
+//                        // Display Selected date in textbox
+//                        dateInput.setText( (monthOfYear + 1) + "/"
+//                                + dayOfMonth + "/" + year);
+//
+//                    }
+//                }, year, month, day);
+//        dpd.show();
+//    }
+//
+//    public void timePopup(View view){
+//        Calendar cal = Calendar.getInstance();
+//        int hour = cal.get(Calendar.HOUR_OF_DAY);
+//        int minute = cal.get(Calendar.MINUTE);
+//        final int meridian = cal.get(Calendar.AM_PM);
+//
+//        TimePickerDialog timePickerDialog = new TimePickerDialog(
+//                ReservationActivity.this, R.style.DialogTheme, new TimePickerDialog.OnTimeSetListener() {
+//            //private String am_pm;
+//
+//            @Override
+//            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                //if (meridian == Calendar.AM)
+//                //am_pm = "AM";
+//                //else if (meridian == Calendar.PM)
+//                //am_pm = "PM";
+//                timeInput.setText(hourOfDay + " : " + minute + " Hours");
+//            }
+//        }, hour, minute,android.text.format.DateFormat.is24HourFormat(ReservationActivity.this)
+//        );
+//        timePickerDialog.show();
+//    }
+//    public void makeReservation(View view) {
+//        Toast.makeText(this, "Table for Party of: " + partySize.getSelectedItem().toString()
+//                + "\n reserved Date: " + dateInput.getText()
+//                + "\n reserved Time: " + timeInput.getText(), Toast.LENGTH_SHORT).show();
+//        Intent intent = new Intent(this, ReservationHistoryListActivity.class);
+//        startActivity(intent);
+//    }
+//
+//
+//}
