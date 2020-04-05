03df5423981a2a41da4d027628cd71a974a914ed
Roman <romanmbwasi@gmail.com>
2020-04-05 04:29:46 -0400

    Formating

--- app/src/main/java/com/mbwasi/a2docapstone/ProfileActivity.java
+++ app/src/main/java/com/mbwasi/a2docapstone/ProfileActivity.java
@@ -35,11 +35,9 @@ import pk.codebase.requests.HttpRequest;
 import pk.codebase.requests.HttpResponse;
 
 public class ProfileActivity extends BaseActivity {
-
     EditText firstName;
     EditText email;
     EditText phone;
-
     ImageView image;
 
     public static final String TAG = "ProfileActivity";
@@ -54,16 +52,14 @@ public class ProfileActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        firstName = (EditText)findViewById(R.id.fName);
-        email = (EditText)findViewById(R.id.email);
-        phone = (EditText)findViewById(R.id.address);
-        image = (ImageView)findViewById(R.id.Pic);
-
+        firstName = (EditText) findViewById(R.id.fName);
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.address);
+        image = (ImageView) findViewById(R.id.Pic);
 
         firstName.setEnabled(false);
         email.setEnabled(false);
         phone.setEnabled(false);
-
 
         //HTTP POST request
         HttpRequest request = new HttpRequest();
@@ -73,52 +69,40 @@ public class ProfileActivity extends BaseActivity {
                 if (response.code == HttpResponse.HTTP_OK) {
 
                     try {
-                        Log.e(TAG, "JSONObject: "+ response.toJSONObject().toString());
-
+                        Log.e(TAG, "JSONObject: " + response.toJSONObject().toString());
 
                         JSONObject successObject = response.toJSONObject().getJSONObject("success");
 
+                        currentUser.setId(successObject.getInt("id"));
+                        currentUser.setName(successObject.getString("name"));
+                        currentUser.setEmail(successObject.getString("email"));
+                        currentUser.setEmail_verified_at(successObject.getString("email_verified_at"));
+                        currentUser.setRole(successObject.getInt("role"));
+                        currentUser.setPhone(successObject.getString("phone"));
+                        currentUser.setAvatar(successObject.getString("avatar"));
+                        currentUser.setStatus(successObject.getInt("status"));
+                        currentUser.setCreated_at(successObject.getString("created_at"));
+                        currentUser.setUpdated_at(successObject.getString("updated_at"));
+                        currentUser.setDeleted_at(successObject.getString("deleted_at"));
 
+                        Log.e(TAG, "Name: " + currentUser.getAvatar());
 
-                    currentUser.setId(successObject.getInt("id"));
-                    currentUser.setName(successObject.getString("name"));
-                    currentUser.setEmail(successObject.getString("email"));
-                    currentUser.setEmail_verified_at(successObject.getString("email_verified_at"));
-                    currentUser.setRole(successObject.getInt("role"));
-                    currentUser.setPhone(successObject.getString("phone"));
-                    currentUser.setAvatar(successObject.getString("avatar"));
-                    currentUser.setStatus(successObject.getInt("status"));
-                    currentUser.setCreated_at(successObject.getString("created_at"));
-                    currentUser.setUpdated_at(successObject.getString("updated_at"));
-                    currentUser.setDeleted_at(successObject.getString("deleted_at"));
+                        firstName.setText(currentUser.getName());
+                        email.setText(currentUser.getEmail());
+                        phone.setText(currentUser.getPhone());
 
-                    Log.e(TAG, "Name: " + currentUser.getAvatar());
-
-                    firstName.setText(currentUser.getName());
-                    email.setText(currentUser.getEmail());
-                    phone.setText(currentUser.getPhone());
-
-                    Picasso.get().load("http://2doo.ca/storage/avatars/"+currentUser.getAvatar()).into(image);
-
-
-
-
-
-
-
+                        Picasso.get().load("http://2doo.ca/storage/avatars/" + currentUser.getAvatar()).into(image);
 
                     } catch (JSONException e) {
                         e.printStackTrace();
                         Log.e(TAG, e.toString());
                     }
-                }
-
-                else if (response.code == HttpResponse.HTTP_UNAUTHORIZED) {
+                } else if (response.code == HttpResponse.HTTP_UNAUTHORIZED) {
 
                     Toasty.error(getApplicationContext(), "Invalid Token", Toast.LENGTH_LONG, true).show();
                 }
                 //Any other HTTP status
-                else{
+                else {
                     Log.e(TAG, "Response code:" + response.code);
 
                     Toasty.error(getApplicationContext(), "Network Error", Toast.LENGTH_LONG, true).show();
@@ -135,7 +119,6 @@ public class ProfileActivity extends BaseActivity {
             }
         });
 
-
         JSONObject json;
         try {
             json = new JSONObject();
@@ -144,76 +127,51 @@ public class ProfileActivity extends BaseActivity {
             return;
         }
         HttpHeaders headers = new HttpHeaders("Authorization", "Bearer " + TokenUtils.getLoginToken(getApplicationContext()));
-        request.post("http://2doo.ca/api/user/details", json,headers);
-
-
-
+        request.post("http://2doo.ca/api/user/details", json, headers);
 
     }
 
     public void editProfile(View view) {
-
         firstName.setEnabled(true);
         email.setEnabled(true);
         phone.setEnabled(true);
-
     }
 
     public void saveProfile(View view) {
-
         firstName.setEnabled(false);
         email.setEnabled(false);
         phone.setEnabled(false);
-
-
     }
 
-
-
     public void editPic(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-
-            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
 
                 String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
 
                 requestPermissions(permissions, PERMISSION_CODE);
-            }else{
+            } else {
                 pickImageFromGallery();
             }
-
-
-
-
-        }else{
-
+        } else {
             pickImageFromGallery();
-
         }
-
     }
 
-
-
     private void pickImageFromGallery() {
-
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, IMAGE_PICK_CODE);
-
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case PERMISSION_CODE: {
-
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     pickImageFromGallery();
-                }else{
-
+                } else {
                     Toasty.error(getApplicationContext(), "Permission Denied!", Toast.LENGTH_LONG, true).show();
                 }
             }
@@ -222,13 +180,10 @@ public class ProfileActivity extends BaseActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
-
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-
             image.setImageURI(data.getData());
         }
     }
-    }
+}
 
