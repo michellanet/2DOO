fc048e1818b67e7d49e44d4e4623c6025c01903e
Deep Desai <Deepdesai36@gmail.com>
2020-04-06 15:57:26 -0400

    Finished Forgot Password Activity & Made Some GUI Changes

--- app/src/main/java/com/mbwasi/a2docapstone/ForgotPassword_Activity.java
+++ app/src/main/java/com/mbwasi/a2docapstone/ForgotPassword_Activity.java
@@ -1,12 +1,18 @@
 package com.mbwasi.a2docapstone;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ForgotPassword_Activity extends AppCompatActivity {
+
+    EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +21,54 @@ public class ForgotPassword_Activity extends AppCompatActivity {
     }
 
     public void resetClicked(View view) {
+
+        email = findViewById(R.id.emailResetET);
+        String email1 = email.getText().toString();
+
+        if(email1.isEmpty()){
+
+            new AlertDialog.Builder(this)
+                    .setTitle("ERROR")
+                    .setMessage("CannDeot Leave E-Mail Empty!")
+
+
+                    .setNegativeButton(android.R.string.ok, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+
+
+        }else {
+
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Request Password Change")
+                    .setMessage("Are you sure you want to reset your password??")
+
+
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Toast.makeText(getApplicationContext(), "Check Your E-Mail for Password Reset", Toast.LENGTH_LONG).show();
+
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+
+
+
+        }
+
     }
 
     public void registerPressed(View view) {
         Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void loginRedirectPressed(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
         startActivity(intent);
     }
 }
